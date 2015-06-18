91e51c3c6fbb687b6912e04db0358e392ffa869a
Kirkpatrick <kirkp1ia@aux-vm-bb9a98.central.cmich.local>
2015-06-18 10:19:06 -0400

    _

--- src/frontEnd/ThemeScreen.java
+++ src/frontEnd/ThemeScreen.java
@@ -24,13 +24,28 @@ public class ThemeScreen extends AnchorPane {
 			this.value = FXMLLoader.load(this.getClass().getResource("../frontEnd/fxml/ThemeScreen.fxml"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			
+		} catch (NullPointerException n){
+			try{
+				this.value = FXMLLoader.load(this.getClass().getResource("/frontEnd/fxml/ThemeScreen.fxml"));
+			} catch (IOException e1){
+				e1.printStackTrace();
+			}
 		}
 		
 		this.value.setBackground(new Background(new BackgroundImage(new Image(getClass().getResourceAsStream("/staticFiles/themes/" + theme + "/background.jpeg")), null, null, null, new BackgroundSize(100, 450, true, false, true, true))));
 		
 		this.getChildren().add(this.value);
 		
-		this.getStylesheets().add(getClass().getResource("../staticFiles/themes/" + theme + "/css/style.css").toString());
+		try{
+			this.getStylesheets().add(getClass().getResource("../staticFiles/themes/" + theme + "/css/style.css").toString());
+		} catch(Exception e){
+			try{
+				this.getStylesheets().add(getClass().getResource("/staticFiles/themes/" + theme + "/css/style.css").toString());
+			}
+			catch(Exception e1){
+				e1.printStackTrace();
+			}
+		}
 	}
 }
