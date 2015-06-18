117e9cdc1e0923fba894da0e3f654f2d0f61835f
Kirkpatrick <kirkp1ia@aux-vm-bb9a98.central.cmich.local>
2015-06-18 10:07:16 -0400

    finished

--- src/frontEnd/controllers/ThemeScreenController.java
+++ src/frontEnd/controllers/ThemeScreenController.java
@@ -3,10 +3,16 @@ package frontEnd.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import javafx.animation.FadeTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.effect.Glow;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 public class ThemeScreenController implements Initializable {
 
@@ -20,4 +26,62 @@ public class ThemeScreenController implements Initializable {
 
 	}
 
+	@FXML public void chooseGold(){
+		Main.newGame("gold");
+	}
+	
+	@FXML public void chooseBasic(){
+		Main.newGame("basic");
+	}
+	
+	
+	SequentialTransition fadeTransition;
+	@FXML public void basicGlow(){
+		basic.setEffect(new Glow(0.8));
+
+		fadeTransition = new SequentialTransition();
+
+		FadeTransition fadeOut = getFadeTransition(basic,
+				basic.getOpacity(), 0.0, 800);
+		FadeTransition fadeIn = getFadeTransition(basic, 0.0, 1.0, 800);
+
+		fadeTransition.getChildren().addAll(fadeOut, fadeIn);
+		fadeTransition.setCycleCount(Timeline.INDEFINITE);
+		fadeTransition.play();
+	}
+	
+	@FXML public void stopGlow(){
+		fadeTransition.stop();
+		basic.setOpacity(1.0);
+		basic.setEffect(null);
+		gold.setOpacity(1.0);
+		gold.setEffect(null);
+	}
+	
+	@FXML public void goldGlow(){
+		gold.setEffect(new Glow(0.8));
+
+		fadeTransition = new SequentialTransition();
+
+		FadeTransition fadeOut = getFadeTransition(gold,
+				gold.getOpacity(), 0.0, 800);
+		FadeTransition fadeIn = getFadeTransition(gold, 0.0, 1.0, 800);
+
+		fadeTransition.getChildren().addAll(fadeOut, fadeIn);
+		fadeTransition.setCycleCount(Timeline.INDEFINITE);
+		fadeTransition.play();
+	}
+	
+	public FadeTransition getFadeTransition(ImageView imageView,
+			double fromValue, double toValue, int durationInMilliseconds) {
+
+		FadeTransition ft = new FadeTransition(Duration
+				.millis(durationInMilliseconds), imageView);
+		ft.setFromValue(fromValue);
+		ft.setToValue(toValue);
+
+		return ft;
+
+	}
+	
 }
